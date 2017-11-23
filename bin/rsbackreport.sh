8a572c015e82b4cc85ba5165af710edfb5b3576b
ballestr <ballestr@users.noreply.github.com>
2017-11-23 12:47:50 +0100

    special handling of configtest, avoid status file

--- bin/rsbackreport.sh
+++ bin/rsbackreport.sh
@@ -8,4 +8,9 @@
 CFGB=$1
 status=$2
 [ "$CFGB" ] || exit 1
-printf "%-4s %s\n" "$status"  "$(date '+%Y-%m-%d %H:%M') $CFGB [Remote $SSH_CLIENT]" >$LOGDIR/$CFGB.status
+if [ "$CFGB" = "configtest" ] ; then
+    echo "rsbackreport.sh $CFGB from $SSH_CLIENT OK $(date '+%Y-%m-%d %H:%M')"
+    exit 0
+else
+    printf "%-4s %s\n" "$status"  "$(date '+%Y-%m-%d %H:%M') $CFGB [Remote $SSH_CLIENT]" >$LOGDIR/$CFGB.status
+fi
