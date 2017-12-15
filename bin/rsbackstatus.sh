a27db672b7e7a0e62c5918688458c0e5d0c3a580
ballestr <ballestr@users.noreply.github.com>
2017-12-15 09:46:50 +0100

    support exit codes etc for nagios/icinga servicecheck

--- bin/rsbackstatus.sh
+++ bin/rsbackstatus.sh
@@ -1,59 +1,71 @@
 #!/opt/bin/bash
 ## send status mail for rsbak
 ## Sergio.Ballestero@gmail.com January 2017
+## support exit codes etc for nagios/icinga servicecheck
+## Sergio.Ballestero@gmail.com December 2017
 
 . /opt/rsbak/etc/rsbackup.rc || exit 1
 
 ## add to cron path
 PATH="/usr/sbin:/sbin:$PATH"
 
-function rsback_status {
-status="OK"
-shopt -s nullglob
-F=$LOGDIR/*.status
-if ls $F >/dev/null ; then
-  if grep -q FAIL $LOGDIR/*.status ; then
-    status="FAIL"
-  fi
-else
-   status="NOSTATUS"
-fi
-stale=$(find $LOGDIR -name "*.status" -mmin +$[60*$FRESHMAX+60])
-if [ "$stale" ]; then
-    status="STALE $status"
-fi
-stalelog=$(find $LOGDIR -name "*.log" -mmin +$[60*$FRESHMAX+60])
-if [ "$stalelog" ]; then
-    status="STALELOG $status"
-fi
+function rsback_status() {
+    local MODE=$1
+    local status="OK"
+    local level="notice"
+    shopt -s nullglob
+    local F=$LOGDIR/*.status
+    if ls $F >/dev/null ; then
+        if grep -q FAIL $LOGDIR/*.status ; then
+            status="FAIL"
+        fi
+    else
+        status="NOSTATUS"
+    fi
+    stale=$(find $LOGDIR -name "*.status" -mmin +$[60*$FRESHMAX+60])
+    if [ "$stale" ]; then
+        status="STALE $status"
+    fi
+    stalelog=$(find $LOGDIR -name "*.log" -mmin +$[60*$FRESHMAX+60])
+    if [ "$stalelog" ]; then
+        status="STALELOG $status"
+    fi
+    [ $status = "OK" ] || level="error"
+    logger -t rsbackup -p user.$level "status check $STATUS"
 
-logger -t rsbackup -p user.warn "status check $STATUS"
+    ## Mail headers
+    [ "$MODE" = "--mail" ] && echo -e "From: $FROM\nTo: $MAILTO\nSubject: [RSBAK/$HOST] status $status\n"
 
-## Mail headers
-[ "$1" = "-m" ] && echo -e "From: $FROM\nTo: $MAILTO\nSubject: [RSBAK/$HOST] status $status\n"
-
-echo "$(date) Checking status files in $HOST:$LOGDIR : $status"
-if [ "${stale}${stalelog}" ]; then
-    echo
-    echo "** ERROR: Stale status/log files found (>$FRESHMAX hours):"
-    for f in $stale $stalelog; do
-      ls -la $f
-      cat $f
-    done
-    echo "**"
-    echo
-fi
-echo "** Status files content:"
-ls $LOGDIR/*.status >/dev/null  && cat $LOGDIR/*.status
-if [ "$DEBUG" ]; then
-  echo
-  echo "** DEBUG: Files in $LOGDIR:"
-  (cd $LOGDIR && ls -la *.status)
-fi
+    if [ "$MODE" = "--nagios" ]; then
+        echo "$status rsbackup $HOST:$LOGDIR :"
+    else
+        echo "$(date) Checking status files in $HOST:$LOGDIR : $status"
+    fi
+    if [ "${stale}${stalelog}" ]; then
+        echo
+        echo "** ERROR: Stale status/log files found (>$FRESHMAX hours):"
+        for f in $stale $stalelog; do
+            ls -la $f
+            cat $f
+        done
+        echo "**"
+        echo
+    fi
+    echo "** Status files content:"
+    ls $LOGDIR/*.status >/dev/null  && cat $LOGDIR/*.status
+    if [ "$MODE" = "--nagios" ]; then
+        [ $status = "OK" ] || return 1
+        return 0
+    fi
+    if [ "$DEBUG" ]; then
+        echo
+        echo "** DEBUG: Files in $LOGDIR:"
+        (cd $LOGDIR && ls -la *.status)
+    fi
 }
 
-if [ "$1" = "-m" ]; then
-  rsback_status -m  2>&1 | sendmail $MAILTO
+if [ "$1" = "-m" -o "$1" = "--mail" ]; then
+    rsback_status --mail  2>&1 | sendmail $MAILTO
 else
-  rsback_status
+    rsback_status $1
 fi
