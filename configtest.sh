a417f6c9bad102d10d58517f9e33a470c7b781d8
Sergio Ballestrero <sash@cern.ch>
2017-12-20 17:08:08 +0100

    check also logfile and lockfile, and LOGDIR setting

--- configtest.sh
+++ configtest.sh
@@ -8,24 +8,47 @@
 
 DIR=/opt/rsbak
 SSHCFG=$DIR/etc/ssh.config
+R=0
 
 #ls -la /opt/rsbak || exit
 cd $DIR || exit
 
 [ -d etc ] || { echo "## configtest: no etc configuration directory"; exit 1; }
 [ -f etc/rsbackup.rc ] || { echo "## configtest: no etc/rsbackup.rc file"; exit 1; }
+eval $(grep ^LOGDIR etc/rsbackup.rc)
+if [ -z "$LOGDIR" ] ; then
+    echo "### LOGDIR not defined in etc/rsbackup.rc"
+    R=$[R+1]
+else
+    echo "### configtest: LOGDIR=$LOGDIR"
+fi
 
 RSA=/root/.ssh/id_rsa_rsbackup
 if ! [ -f $RSA ] ; then
-    echo "missing ssh key $RSA"
+    echo "### missing ssh key $RSA"
     R=$[R+1]
+else
+    echo "### configtest: ssh key $(ls -la $RSA)"
 fi
 
-R=0
+## check parname value in config file
+function cfgcheck() {
+    local parname=$1
+    local refval=$2
+    local bf=$(basename $cfg .conf)
+    local parval=$(grep ^$parname $cfg | sed "s/$parname[[:space:]]*//")
+    if [ "$parval" != "$refval" ]; then
+	echo "### checking $parname $refval : not matched '$parval'"
+	egrep -n "$parname" $cfg | sed 's/^/  /'
+	R=$[R+1]
+    fi
+}    
+
 N=0
 shopt -s nullglob
 for cfg in etc/rsnapshot.*.conf; do
     N=$[N+1]
+    bf=$(basename $cfg .conf)
     echo -n "## Checking $(basename $cfg):  "
     rsnapshot -v -c $cfg configtest
     r=$?
@@ -35,14 +58,21 @@ for cfg in etc/rsnapshot.*.conf; do
 	echo "### checking snapshot_root '$DIR' : not present or not a directory"
 	R=$[R+1]
     fi
+    ## check lockfile
+    lockdir="/var/run"
+    cfgcheck lockfile "$lockdir/$bf.pid"
+    ## check logfile
+    cfgcheck logfile "$LOGDIR/$bf.log"
+    
     ## check SSH
     sshdest=$(grep ^backup $cfg | grep @ | cut -f2 | cut -d: -f1 | sort | uniq)
     if [ "$sshdest" ]; then
         echo -n "### checking config $cfg for ssh_args containing '-F $SSHCFG': "
+        ## don't use cfgcheck, allow additional options after the -F 
 	egrep -q "^ssh_args.*-F $SSHCFG" $cfg
 	if [ $? -ne 0 ]; then
 	    echo "failed:"
-	    egrep "ssh_args" $cfg
+	    egrep -n "ssh_args" $cfg | sed 's/^/  /'
 	    R=$[R+1]
 	else
 	    echo "OK"
