fa793647d8c5110e9e9457d0c2020e545c870dea
ballestr <ballestr@users.noreply.github.com>
2017-12-21 01:33:06 +0100

    use /opt/bin/bash; add retain, in addition to interval

--- configsummary.sh
+++ configsummary.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/opt/bin/bash
 # synoptic summary of configurations
-for par in snapshot_root logfile loglevel link_dest sync_first no_create_root interval ssh_args exclude_file rsync_short_args rsync_long_args; do
+for par in snapshot_root logfile loglevel link_dest sync_first no_create_root interval retain ssh_args exclude_file rsync_short_args rsync_long_args; do
     for cfg in etc/rsnapshot.*.conf; do
     printf "%-50s:" $cfg
     grep  "^$par" $cfg || echo "$par (not found)"
